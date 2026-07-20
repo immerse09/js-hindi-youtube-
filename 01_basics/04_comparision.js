95a6e0d17412c52516979e4f14f852f61e4fbb34
immerse09 <nayakaman7172@gmail.com>
2026-07-20 16:28:36 +0000

    containet config done

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -4,18 +4,21 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
+// problem occure when compare whith different datatype
+// so whenever you compare make sure the datatype should be same
+// coz. typescript never allow to compare with differnt datatype
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1); true
+// console.log("02" > 1); true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
 
-// === 
+// === (strict check)
 
 console.log("2" === 2);
