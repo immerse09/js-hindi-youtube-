f9933fb12a71fca4a447b8781ea26bb3dcebbacc
immerse09 <nayakaman7172@gmail.com>
2026-07-21 19:26:13 +0000

    container config done

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,8 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length); // first convert in string then it give length
+console.log(balance.toString().to)
