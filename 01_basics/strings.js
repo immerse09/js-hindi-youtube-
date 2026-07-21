0149d12d04f5906430b045f9c1974323428e2e63
immerse09 <nayakaman7172@gmail.com>
2026-07-21 18:43:27 +0000

    container config done

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -8,7 +8,7 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
 
 // another way to initialize string
-const gameName = new String('aman')
+const gameName = new String('aman-nayak')
 
 // console.log(gameName[0]); //It will give (key value pair) // a
 // at all key value it not give position it is actually object
@@ -19,3 +19,28 @@ const gameName = new String('aman')
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2)); // a
 console.log(gameName.indexOf('a')); // 0
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "     aman.   "
+console.log(newStringOne); //       aman.        
+//trim
+console.log(newStringOne.trim()); // aman
+
+const url = "https://aman.com/aman%20nayak"
+
+//replace
+console.log(url.replace('%20', '-'))  // https://aman.com/aman-nayak
+
+// check
+console.log(url.includes('sundar')) // false
+console.log(url.includes('aman')) // true
+
+// to convert string into array
+console.log(gameName.split()); // - , . etc
+
+// Today's task is to go to mdn and read string prototype
