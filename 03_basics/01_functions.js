ff14aa68ddbb27e285e5f0000969fa92da0d17c6
immerse09 <nayakaman7172@gmail.com>
2026-08-28 16:40:34 +0000

    container config done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,22 +34,32 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("aman"))
 
 
+// ... -> rest operator also called spread operator based on use case
 function calculateCartPrice(val1, val2, ...nnum1){
     return num1
 }
 
-console.log(calculateCartPrice(200, 400, 500, 2000))
+// console.log(calculateCartPrice(200, 400, 500, 2000)) 
 
+// object
 const user = {
     username: "aman",
     prices:99
 }
 
+// passing any object in function
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is $ {anyobject.price}`);
+}
+
+// we can pass object directly as:
+// handleObject(user)
 handleObject({
     username: "sam",
-    price: 499
+    price: 399
 })
 
+//  just passing array
 const myNewArray = [200, 400, 100, 600]
 
 function returnSecondValue(geetArray){
@@ -57,3 +67,4 @@ function returnSecondValue(geetArray){
 }
 
 console.log(returnSecondValue([200, 400, 500, 1000]));
+// console.log(returnSecondValue(myNewArray)); // also can pass variable
