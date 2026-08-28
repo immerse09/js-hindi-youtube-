8040f1e35dcf84eef2b70039b83aed363f0faf10
immerse09 <nayakaman7172@gmail.com>
2026-08-28 17:00:33 +0000

    container config done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,4 +1,6 @@
 //var c = 300
+// {} scope
+
 let a = 300
 if (true) {
     let a = 10
@@ -7,13 +9,16 @@ if (true) {
     
 }
 
+// note: we can use global scope variable in local scope
+//  but we cannot use local scope variable as globally
+
 
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
 
-
+// nested scope or function
 function one(){
     const username = "hitesh"
 
@@ -33,18 +38,18 @@ if (true) {
     const username = "hitesh"
     if (username === "hitesh") {
         const website = " youtube"
-        // console.log(username + website);
+        // console.log(username + website); // hitesh youtube
     }
-    // console.log(website);
+    // console.log(website); // not in scope
 }
 
-// console.log(username);
+// console.log(username); // not in scope
 
 
 // ++++++++++++++++++ interesting ++++++++++++++++++
 
 
-console.log(addone(5))
+console.log(addone(5)) // 6
 
 function addone(num){
     return num + 1
@@ -52,7 +57,7 @@ function addone(num){
 
 
 
-addTwo(5)
+addTwo(5) // error, coz func. is hold in variable , so we'll study 'hoisting'
 const addTwo = function(num){
     return num + 2
 }
