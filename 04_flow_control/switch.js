41343f4a41dbabd535c435b10a1b0579cb9d9c11
immerse09 <nayakaman7172@gmail.com>
2026-08-30 13:06:43 +0000

    container config done

---
+++ 04_flow_control/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+const month = 3
+
+switch(month) {
+case 1:
+console.log("January");
+break; // to break the flow 
+case 2:
+console.log("February");
+break;
+case 3:
+console.log("March");
+break;
+case 4:
+console.log("April");
+break; //
+
+default:
+    console.log("defalut case math");
+    break;
+}
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
